c32ec463b0e623f37f5a0d0f14116c9f5bc82b3c
jonathan.mandombe <jonathan.mandombe@ideiasdinamicas.com>
2022-12-01 15:14:04 +0100

    First Step

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "main",
+    component: AppComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+        // loadChildren: () => import('./app.module').then(m => m.AppModule)
+      },
+      // {
+      //   path: 'comission',
+      //   component: ListComponent,
+      //   loadChildren: () => import('./views/private/comission/comission.module').then(m => m.ComissionModule)
+      // },
+      // {
+      //   path: 'comission/create',
+      //   component: CreateComponent,
+      //   loadChildren: () => import('./views/private/comission/comission.module').then(m => m.ComissionModule)
+      // }
+    ]
+  },
+  {
+    path: "",
+    component: HomeComponent
+  },
+  // {
+  //   path: '**',
+  //   component: NotFoundComponentComponent
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
